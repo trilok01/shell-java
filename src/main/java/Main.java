b90f9348849a039f1c4c6333d72c8a4f5a4eb224
Trilok <65667915+trilok01@users.noreply.github.com>
2025-01-29 20:22:11 +0000

    Print user input

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,5 +7,7 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
+        
+        System.out.println(input + ": command not found");
     }
 }
