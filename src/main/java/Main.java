f0a996fa71abfef76cd83c313a94c500f4308bb4
Trilok <65667915+trilok01@users.noreply.github.com>
2025-01-30 11:43:06 +0000

    REPL

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,12 +2,15 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        // Uncomment this block to pass the first stage
-        System.out.print("$ ");
-
+        // Uncomment this block to pass the first stage        
         Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
         
-        System.out.println(input + ": command not found");
+        while(true) {
+        	System.out.print("$ ");
+        	
+        	String input = scanner.nextLine();
+            
+            System.out.println(input + ": command not found");
+        }
     }
 }
