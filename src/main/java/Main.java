1364e9d4b5b7d7c79b54ed892fdad1f78ed3818b
Trilok <65667915+trilok01@users.noreply.github.com>
2025-02-03 20:03:36 +0000

    Debugging

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -28,12 +28,6 @@ public class Main {
         	} else if(input.startsWith("type ")) {
         		String command = input.substring(5);
         		
-//        		ONLY FOR DEBUGGING. REMOVE AFTER FIXING THE BUG. STARTS HERE
-        		
-        		System.out.println("args length -----> " + args.length);
-        		
-//        		ONLY FOR DEBUGGING. REMOVE AFTER FIXING THE BUG. ENDS HERE
-        		
         		if(builtinCommands.contains(command)) {
         			System.out.println(command + " is a shell builtin");
         			
@@ -53,7 +47,7 @@ public class Main {
     	String resultPath = null;
     	
     	if(args.length == 0) {
-    		File file = new File(fileName);
+    		File file = new File("/bin/");
     		fileExists = file.exists();
     		resultPath = fileExists ? file.getAbsolutePath() : null;
     		
@@ -65,24 +59,6 @@ public class Main {
         		
 //        		Check if any executable file exists in the given directory with the name of command
         		fileExists = new File(path, fileName).exists();
-        		
-//        		ONLY FOR DEBUGGING. REMOVE AFTER FIXING THE BUG. STARTS HERE
-        		
-        		System.out.println("path -----> " + path + "\ncommand -----> " + fileName + "\nfileExists -----> " + fileExists);
-        		
-        		File[] listOfFiles = new File(path).listFiles();
-        		
-        		if(listOfFiles != null) {
-        			for (int i = 0; i < listOfFiles.length; i++) {
-        				if (listOfFiles[i].isFile()) {
-        					System.out.println("File -----> " + listOfFiles[i].getName());
-        				} else if (listOfFiles[i].isDirectory()) {
-        					System.out.println("Directory -----> " + listOfFiles[i].getName());
-        				}
-        			}
-        		}
-        		
-//        		ONLY FOR DEBUGGING. REMOVE AFTER FIXING THE BUG. ENDS HERE                		
         		
         		if(fileExists) {
         			resultPath = path;
