2bafb7e98172563642735aef7f3e16909238197d
Trilok <65667915+trilok01@users.noreply.github.com>
2025-02-03 15:11:44 +0000

    type command added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,16 @@
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Main {
+	
     public static void main(String[] args) throws Exception {
-        // Uncomment this block to pass the first stage        
         Scanner scanner = new Scanner(System.in);
+        
+        Set<String> commands = new HashSet<String>();
+        commands.add("type");
+        commands.add("exit");
+        commands.add("echo");
         
         while(true) {
         	System.out.print("$ ");
@@ -12,8 +19,16 @@ public class Main {
             
         	if(input.equals("exit 0")) {
         		break;
+        		
         	} else if(input.startsWith("echo ")) {
         		System.out.println(input.replace("echo ", ""));
+        		
+        	} else if(input.startsWith("type ")) {
+        		String command = input.substring(5);
+        		String message = command + (commands.contains(command) ? " is a shell builtin" : ": not found");
+        		
+        		System.out.println(message);
+        		
         	} else {
         		System.out.println(input + ": command not found");
         	}
